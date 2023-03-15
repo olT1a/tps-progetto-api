b690770ea93bc7aec4236f5239bd326e5d08254a
olT1a <mattia.rocchi2004@gmail.com>
2023-03-15 18:13:35 +0100

    get weather info

--- Rocchi_meteo/Rocchi_meteo/Form1.cs
+++ Rocchi_meteo/Rocchi_meteo/Form1.cs
@@ -22,6 +22,8 @@ namespace Rocchi_meteo
             InitializeComponent();
         }
         static HttpClient client = new HttpClient();
+        Rootobject_coordinates coordinates = new Rootobject_coordinates();
+        int i = 0;
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -30,27 +32,44 @@ namespace Rocchi_meteo
 
         private async void btn_cerca_Click(object sender, EventArgs e)
         {
-            await get();
+            await get_coordinates();
+            await get_weather_info();
         }
-        public async Task<Rootobject> get()
+        public async Task<Rootobject_coordinates> get_coordinates()
         {
-            Rootobject r = new Rootobject();
-            int i = 0;
+
             string url = $"https://geocoding-api.open-meteo.com/v1/search?name={txt_citta.Text}&language=it";
+
             HttpResponseMessage response = await client.GetAsync(url);
             MessageBox.Show(response.ToString());
             if (response.IsSuccessStatusCode)
             {
 
-                r = await JsonSerializer.DeserializeAsync<Rootobject>(await response.Content.ReadAsStreamAsync());
-                MessageBox.Show(r.results[i].latitude + " " + r.results[i].longitude);
+                coordinates = await JsonSerializer.DeserializeAsync<Rootobject_coordinates>(await response.Content.ReadAsStreamAsync());
+                MessageBox.Show(Math.Round(coordinates.results[i].latitude,2).ToString() + " " + Math.Round(coordinates.results[i].longitude,2).ToString());
             }
-            return r;
+       
+            return coordinates;
 
+        }
+        public async Task<Rootobject_weather_info> get_weather_info()
+        {
+            Rootobject_weather_info weather_Info = new Rootobject_weather_info();
+            string url = $"https://api.open-meteo.com/v1/forecast?latitude={ Math.Round(coordinates.results[i].latitude,2).ToString().Replace(',', '.') }&longitude={ Math.Round(coordinates.results[i].longitude,2).ToString().Replace(',', '.') }&hourly=temperature_2m ";
+            
+            HttpResponseMessage response = await client.GetAsync(url);
+            MessageBox.Show(response.ToString());
+            if (response.IsSuccessStatusCode)
+            {
+
+                weather_Info = await JsonSerializer.DeserializeAsync<Rootobject_weather_info>(await response.Content.ReadAsStreamAsync());
+                MessageBox.Show(weather_Info.hourly.temperature_2m[1].ToString());
+            }
+            return weather_Info;
         }
     }
 
-    public class Rootobject
+    public class Rootobject_coordinates
     {
         public Result[] results { get; set; }
     }
@@ -78,5 +97,41 @@ namespace Rocchi_meteo
         public string admin3 { get; set; }
         public string admin4 { get; set; }
     }
+
+
+    public class Rootobject_weather_info
+    {
+        public float latitude { get; set; }
+        public float longitude { get; set; }
+        public float elevation { get; set; }
+        public float generationtime_ms { get; set; }
+        public int utc_offset_seconds { get; set; }
+        public string timezone { get; set; }
+        public string timezone_abbreviation { get; set; }
+        public Hourly hourly { get; set; }
+        public Hourly_Units hourly_units { get; set; }
+        public Current_Weather current_weather { get; set; }
+    }
+
+    public class Hourly
+    {
+        public string[] time { get; set; }
+        public float[] temperature_2m { get; set; }
+    }
+
+    public class Hourly_Units
+    {
+        public string temperature_2m { get; set; }
+    }
+
+    public class Current_Weather
+    {
+        public string time { get; set; }
+        public float temperature { get; set; }
+        public int weathercode { get; set; }
+        public float windspeed { get; set; }
+        public float winddirection { get; set; }
+    }
+
 
 }

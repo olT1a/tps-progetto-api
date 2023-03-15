be87cebeb74806b9634072640a6d9849e05ef92d
olT1a <mattia.rocchi2004@gmail.com>
2023-03-15 16:27:28 +0100

    get coordinates

---
+++ Rocchi_meteo/Rocchi_meteo/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Rocchi_meteo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_citta = new System.Windows.Forms.TextBox();
+            this.lbl_citta = new System.Windows.Forms.Label();
+            this.btn_cerca = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_citta
+            // 
+            this.txt_citta.Location = new System.Drawing.Point(23, 50);
+            this.txt_citta.Name = "txt_citta";
+            this.txt_citta.Size = new System.Drawing.Size(100, 22);
+            this.txt_citta.TabIndex = 0;
+            // 
+            // lbl_citta
+            // 
+            this.lbl_citta.AutoSize = true;
+            this.lbl_citta.Location = new System.Drawing.Point(20, 31);
+            this.lbl_citta.Name = "lbl_citta";
+            this.lbl_citta.Size = new System.Drawing.Size(85, 17);
+            this.lbl_citta.TabIndex = 1;
+            this.lbl_citta.Text = "Inserire citta";
+            // 
+            // btn_cerca
+            // 
+            this.btn_cerca.Location = new System.Drawing.Point(23, 78);
+            this.btn_cerca.Name = "btn_cerca";
+            this.btn_cerca.Size = new System.Drawing.Size(75, 23);
+            this.btn_cerca.TabIndex = 2;
+            this.btn_cerca.Text = "cerca";
+            this.btn_cerca.UseVisualStyleBackColor = true;
+            this.btn_cerca.Click += new System.EventHandler(this.btn_cerca_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_cerca);
+            this.Controls.Add(this.lbl_citta);
+            this.Controls.Add(this.txt_citta);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbl_citta;
+        private System.Windows.Forms.Button btn_cerca;
+        public System.Windows.Forms.TextBox txt_citta;
+    }
+}
+
---
+++ Rocchi_meteo/Rocchi_meteo/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Reflection.Emit;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rocchi_meteo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static HttpClient client = new HttpClient();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btn_cerca_Click(object sender, EventArgs e)
+        {
+            await get();
+        }
+        public async Task<Rootobject> get()
+        {
+            Rootobject r = new Rootobject();
+            int i = 0;
+            string url = $"https://geocoding-api.open-meteo.com/v1/search?name={txt_citta.Text}&language=it";
+            HttpResponseMessage response = await client.GetAsync(url);
+            MessageBox.Show(response.ToString());
+            if (response.IsSuccessStatusCode)
+            {
+
+                r = await JsonSerializer.DeserializeAsync<Rootobject>(await response.Content.ReadAsStreamAsync());
+                MessageBox.Show(r.results[i].latitude + " " + r.results[i].longitude);
+            }
+            return r;
+
+        }
+    }
+
+    public class Rootobject
+    {
+        public Result[] results { get; set; }
+    }
+
+    public class Result
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public float latitude { get; set; }
+        public float longitude { get; set; }
+        public float elevation { get; set; }
+        public string feature_code { get; set; }
+        public string country_code { get; set; }
+        public int admin1_id { get; set; }
+        public int admin2_id { get; set; }
+        public int admin3_id { get; set; }
+        public int admin4_id { get; set; }
+        public string timezone { get; set; }
+        public int population { get; set; }
+        public string[] postcodes { get; set; }
+        public int country_id { get; set; }
+        public string country { get; set; }
+        public string admin1 { get; set; }
+        public string admin2 { get; set; }
+        public string admin3 { get; set; }
+        public string admin4 { get; set; }
+    }
+
+}

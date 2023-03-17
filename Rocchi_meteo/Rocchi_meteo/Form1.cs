af3d00ffe5ceeff9d6b33bfeeb17b04fbacffe0f
olT1a <mattia.rocchi2004@gmail.com>
2023-03-17 18:52:17 +0100

    real end

--- Rocchi_meteo/Rocchi_meteo/Form1.cs
+++ Rocchi_meteo/Rocchi_meteo/Form1.cs
@@ -53,7 +53,6 @@ namespace Rocchi_meteo
             if (response.IsSuccessStatusCode)
             {
                 coordinates = await JsonSerializer.DeserializeAsync<Rootobject_coordinates>(await response.Content.ReadAsStreamAsync());
-                //MessageBox.Show(Math.Round(coordinates.results[i].latitude, 2).ToString() + " " + Math.Round(coordinates.results[i].longitude, 2).ToString());
             }
 
             return coordinates;
@@ -68,91 +67,59 @@ namespace Rocchi_meteo
             if (response.IsSuccessStatusCode)
             {
                 weather_Info = await JsonSerializer.DeserializeAsync<Rootobject_weather_info>(await response.Content.ReadAsStreamAsync());
-                set_hours(weather_Info);
+                //set_hours(weather_Info);
                 set_temperature(weather_Info);
-                //int i = 0;
-                //MessageBox.Show(weather_Info.daily.temperature_2m_max[i].ToString());
-                //lbl_c_temperature.Text = weather_Info.current_weather.temperature.ToString();
-                //lbl_time.Text = weather_Info.current_weather.time.ToString();
-                //lbl_windspeed.Text = weather_Info.current_weather.windspeed.ToString();
-
             }
             return weather_Info;
         }
         public void set_temperature(Rootobject_weather_info a)
         {
+            string[] time;
             for (int i = 0; i < a.hourly.temperature_2m.Length; i++)
             {
-                if (i < 24)
-                {
-                    lbl_temp_1.Text += a.hourly.temperature_2m[i].ToString() + "\n";
-                }
-                if (i > 23 && i < 48)
-                {
-                    lbl_temp_2.Text += a.hourly.temperature_2m[i].ToString() + "\n";
-                }
-                if (i > 47 && i < 72)
-                {
-                    lbl_temp_3.Text += a.hourly.temperature_2m[i].ToString() + "\n";
-                }
-                if (i > 71 && i < 96)
-                {   
-                    lbl_temp_4.Text += a.hourly.temperature_2m[i].ToString() + "\n";
-                }
-                if (i > 95 && i < 120)
-                {
-                    lbl_temp_5.Text += a.hourly.temperature_2m[i].ToString() + "\n";
-                }
-                if (i > 119 && i < 144)
-                {
-                    lbl_temp_6.Text += a.hourly.temperature_2m[i].ToString() + "\n";
-                }
-                if (i > 143 && i < 168)
-                {
-                    lbl_temp_7.Text += a.hourly.temperature_2m[i].ToString() + "\n";
-                }
-            }
-        }
-        public void set_hours(Rootobject_weather_info a)
-        {
-            string[] time;
-            for (int i = 0; i < a.hourly.time.Length; i++)
-            {
                 time = a.hourly.time[i].ToString().Split('T');
+
                 if (i < 24)
                 {
                     lbl_data_1.Text = time[0];
                     lbl_ora_1.Text += time[1] + "\n";
+                    lbl_temp_1.Text += a.hourly.temperature_2m[i].ToString() + "\n";
                 }
                 if (i > 23 && i < 48)
                 {
                     lbl_data_2.Text = time[0];
                     lbl_ora_2.Text += time[1] + "\n";
+                    lbl_temp_2.Text += a.hourly.temperature_2m[i].ToString() + "\n";
                 }
                 if (i > 47 && i < 72)
                 {
                     lbl_data_3.Text = time[0];
                     lbl_ora_3.Text += time[1] + "\n";
+                    lbl_temp_3.Text += a.hourly.temperature_2m[i].ToString() + "\n";
                 }
                 if (i > 71 && i < 96)
                 {
                     lbl_data_4.Text = time[0];
                     lbl_ora_4.Text += time[1] + "\n";
+                    lbl_temp_4.Text += a.hourly.temperature_2m[i].ToString() + "\n";
                 }
                 if (i > 95 && i < 120)
                 {
                     lbl_data_5.Text = time[0];
                     lbl_ora_5.Text += time[1] + "\n";
+                    lbl_temp_5.Text += a.hourly.temperature_2m[i].ToString() + "\n";
                 }
                 if (i > 119 && i < 144)
                 {
                     lbl_data_6.Text = time[0];
                     lbl_ora_6.Text += time[1] + "\n";
+                    lbl_temp_6.Text += a.hourly.temperature_2m[i].ToString() + "\n";
                 }
                 if (i > 143 && i < 168)
                 {
                     lbl_data_7.Text = time[0];
                     lbl_ora_7.Text += time[1] + "\n";
+                    lbl_temp_7.Text += a.hourly.temperature_2m[i].ToString() + "\n";
                 }
             }
         }
@@ -179,9 +146,6 @@ namespace Rocchi_meteo
             lbl_ora_5.Text = "";
             lbl_ora_6.Text = "";
             lbl_ora_7.Text = "";
-            /*lbl_c_temperature.Text = "";
-            lbl_time.Text = "";
-            lbl_windspeed.Text = "";*/
         }
 
         private void btn_current_Click(object sender, EventArgs e)
